e8db78381f18e1640eae2d62bfadbff7f7748dd0
YuanJ1ayi <jiyuan@siue.edu>
2024-01-26 23:08:20 -0600

    Add sub.sh script

---
+++ etc/scripts/sub.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+function sub() {
+    read -p "Type the topic_name: " topic_name
+    read -p "Type the file_name.csv: " file_name
+    read -p "Type the rate of data publication (e.g., 1 Hz): " rate
+
+
+    echo "Subscribing to topic: $topic_name"
+    echo "Logging data to: $file_name"
+    echo "Data publication rate: $rate"
+
+ 
+ 
+    previous_topics=(/drone/pose /drone/velocity /drone/battery)
+
+    
+    for topic in "${previous_topics[@]}"; do
+        echo "$topic" >> "$file_name"
+    done
+
+    echo "Subscription and logging completed."
+}
+
+ sub /drone/pose logging.cvs
+
